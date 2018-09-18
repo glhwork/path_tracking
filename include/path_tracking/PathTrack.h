73a6a1a749c14a93d35cc73271d77580a1b7a088
GLINHAI <guilinhai950419@gmail.com>
2018-09-18 19:38:23 +0800

    add class decleration

--- include/path_tracking/PathTrack.h
+++ include/path_tracking/PathTrack.h
@@ -1,8 +1,36 @@
 #ifndef PATHTRACK_H
 #define PATHTRACK_H
 
+#include <vector>
 
+namespace track {
+struct RoboState {
+  RoboState() : v(0) {};
 
+  double x;
+  double y;
+  double z;
+
+  double v;
+};  // struct RoboState
+
+typedef std::vector<RoboState> StateVec;
+
+class PathTrack {
+ public:
+  PathTrack();
+  ~PathTrack();
+  void GetStateCallback();
+  void GetPathCallback();
+  void PubVelocity();
+  void FindV();
+private:
+ RoboState cur_state;
+ StateVec path;
+
+};  // class PathTrack
+
+}  // namespace track
 
 
 
